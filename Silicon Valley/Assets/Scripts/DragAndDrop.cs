c8d5774244714dc2b63d0e7031ad1ca012698225
Johnathan Graybeal <graybealjohnathan@outlook.com>
2021-10-02 16:00:00 -0400

    Update
    Drag and drop + snap works now. Attach script to camera

--- Silicon Valley/Assets/Scripts/DragAndDrop.cs
+++ Silicon Valley/Assets/Scripts/DragAndDrop.cs
@@ -4,33 +4,58 @@ using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
-    public delegate void DragEndedDelegate(DragAndDrop draggableObject);
+    public GameObject objSelected = null;
+    public GameObject[] snapPoints;
+    private float snapSensitivity = 2.0f;
 
-    public DragEndedDelegate dragEndedCallBack;
-    private bool isDragged = false;
-    private Vector3 mouseDragStartPos;
-    private Vector3 spriteDragStartPos;
-
-    private void OnMouseDown()
+    void Update()
     {
-        isDragged = true;
-        mouseDragStartPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        spriteDragStartPos = transform.localPosition;
-    }
-    private void OnMouseDrag()
-    {
-        if (isDragged)
+        //object clicked on
+        if (Input.GetMouseButtonDown(0))
         {
-            transform.localPosition = spriteDragStartPos + (Camera.main.ScreenToWorldPoint(Input.mousePosition) - mouseDragStartPos);
+            CheckHitObject();
+        }
+        //drag an object 
+        if (Input.GetMouseButton(0) && objSelected != null)
+        {
+            DragObject();
+        }
+        //drop object
+        if (Input.GetMouseButtonUp(0) && objSelected != null)
+        {
+            DropObject();
         }
 
+        
     }
-    private void OnMouseUp()
+
+    void CheckHitObject()
     {
-        isDragged = false;
-        dragEndedCallBack(this);
-       
+        RaycastHit2D hit2d = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition));
+        if (hit2d.collider != null)
+        {
+            objSelected = hit2d.transform.gameObject;
+        }
+      
+    }
+
+    
+     void DragObject()
+    {
+        objSelected.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane + 10.0f));
+    }
+
+    void DropObject()
+    {
+        for (int i = 0; i < snapPoints.Length; i++)
+        {
+
+            if (Vector3.Distance(snapPoints[i].transform.position, objSelected.transform.position) < snapSensitivity)
+            {
+                objSelected.transform.position = new Vector3(snapPoints[i].transform.position.x + 0.3f , snapPoints[i].transform.position.y, snapPoints[i].transform.position.z - 0.1f);
+            }
+        }
+        objSelected = null;
     }
 
 }
-    
