2e0a5e808e606b3dac99fcc051a4599ef0aa231b
Danielle P <dnpalmer94@gmail.com>
2021-10-06 21:59:47 -0400

    Updated Version
    This has all the up-to-date code and runs on the newest unity dev soft. It broke the old one  :frowning:

---
+++ Silicon_Valley/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    public GameObject objSelected = null;
+    public GameObject[] snapPoints;
+    private float snapSensitivity = 2.0f;
+
+    void Update()
+    {
+        //object clicked on
+        if (Input.GetMouseButtonDown(0))
+        {
+            CheckHitObject();
+        }
+        //drag an object 
+        if (Input.GetMouseButton(0) && objSelected != null)
+        {
+            DragObject();
+        }
+        //drop object
+        if (Input.GetMouseButtonUp(0) && objSelected != null)
+        {
+            DropObject();
+        }
+
+        
+    }
+
+    void CheckHitObject()
+    {
+        RaycastHit2D hit2d = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition));
+        if (hit2d.collider != null)
+        {
+            objSelected = hit2d.transform.gameObject;
+        }
+      
+    }
+
+    
+     void DragObject()
+    {
+        objSelected.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane + 10.0f));
+    }
+
+    void DropObject()
+    {
+        for (int i = 0; i < snapPoints.Length; i++)
+        {
+
+            if (Vector3.Distance(snapPoints[i].transform.position, objSelected.transform.position) < snapSensitivity)
+            {
+                objSelected.transform.position = new Vector3(snapPoints[i].transform.position.x + 0.3f , snapPoints[i].transform.position.y, snapPoints[i].transform.position.z - 0.1f);
+            }
+        }
+        objSelected = null;
+    }
+
+}
