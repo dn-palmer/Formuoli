bce272a488ce8523b2c36d2f5c26464b78bdd8f2
cpoteet24 <poteetc1@etsu.edu>
2021-10-19 16:19:06 -0400

    Setup screen and game management
    Ability to start a 4 player game. will update to allow for different player counts later. Created proper turn based system (two phases for each player and then cycles). Basic player class and text based resource tracker for the time being.

---
+++ Silicon_Valley/Assets/Scripts/PlayerEntry.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerEntry : MonoBehaviour
+{
+    public Button startGame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = startGame.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void TaskOnClick()
+    {
+        PlayerManager.instance.setPlayers();
+    }
+}
---
+++ Silicon_Valley/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public int numberOfPlayers;
+
+    private ArrayList playerNames;
+
+    public static PlayerManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void setPlayers()
+    {
+        numberOfPlayers = 4;
+    }
+
+}
