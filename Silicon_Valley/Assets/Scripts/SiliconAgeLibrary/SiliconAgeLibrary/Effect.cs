0b7557a8291e7d0a09b2b58851965ce084ceea87
Johnathan Graybeal <graybealjohnathan@outlook.com>
2021-12-05 15:36:09 -0500

    card effect enum
    enumeration for all card effects

---
+++ Silicon_Valley/Assets/Scripts/SiliconAgeLibrary/SiliconAgeLibrary/Effect.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiliconAgeLibrary
+{
+    public enum Effect
+    {
+        AllPlayers,
+        Food,
+        Resources,
+        ProduceResources,
+        Points,
+        Tool,
+        SrDev,
+        Card,
+        TakeTwo
+
+    }
+}
